2a9e471e7693105573c12ebba2e2028886e15c16
pavel cahn <pavel.cahn@gmail.com>
2015-04-07 17:36:57 +0200

    blue

--- app/assets/javascripts/app/help.js
+++ app/assets/javascripts/app/help.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
+  $('.help').hide();
   $('.head-title').click(function(){
-    $('.help').switchClass('help', 'help-active', 1000);
+    $('.help').show('slide', {direction: "up"}, 500, function(){
+    });
+  });
+  $('.back').click(function(){
+    $('.help').hide('slide', {direction: "up"}, 500);
   });
 });

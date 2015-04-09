b6137e61e287a7d6ff79c48098610d1fb4ffb6d0
aliceclv <alice.clavel@edu.escpeurope.eu>
2015-04-09 17:15:37 +0200

    ?

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,8 @@
 class Location < ActiveRecord::Base
   geocoded_by :address
-
   after_validation :geocode, if: :address_changed?
-
   validates :address, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :picture_url, presence: true
+  validates :link, presence: true
 end
